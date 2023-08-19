70b14488abb9329fc145900e245ca025a18bcc40
whb <2028353523@qq.com>
2023-08-19 13:59:29 +0800

    push commit

--- src/main/java/com/wu/controller.java
+++ src/main/java/com/wu/controller.java
@@ -1,5 +1,7 @@
 package com.wu;
 
+
+//连接github 令牌 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
 public class controller {
 
   public static void main(String[] args) {
@@ -8,5 +10,6 @@ public class controller {
     System.out.println("hello git3");
     System.out.println("master test");
     System.out.println("hot-fix test");
+    System.out.println("push test");
   }
 }
